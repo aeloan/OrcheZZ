dcaa618067d7662b83e53af7096525dd7e66710d
eloan.andre <eloan.andre@etu.uca.fr>
2026-05-06 18:46:24 +0200

    Backend : * Ajout de la gestion d'une partition :   * Envoi d'une note au hazard aux clients de la salle   * Reception des audios et calcul des scores   * Fusion des audios de la manche et envoie

--- back/Room.py
+++ back/Room.py
@@ -1,6 +1,11 @@
 import random
 import string
 import threading
+from asyncio import sleep
+
+from back.MusicScoreScorer import compare_notes, mix_audios
+
+notes = ["A", "B", "C", "D", "E", "F", "G"]
 
 
 class Room:
@@ -13,37 +18,59 @@ class Room:
         self.running = False
         self.difficulty = 0
         self.level = 0
+        self.score_round = {}
+        self.score_game = {}
+        self.round_audios = {}
+        self.current_note = None
 
     def add_player(self, client):
-        self.players.append(client)
-        client.set_room(self)
+        if not self.running:
+            self.players.append(client)
+            client.set_room(self)
 
     def remove_player(self, client):
-        if client in self.players:
+        if not self.running and client in self.players:
             self.players.remove(client)
 
     def start_game(self):
         if not self.running:
             self.running = True
+            self.score_game = {p: 0 for p in self.players}
             threading.Thread(target=self.game_loop, daemon=True).start()
 
     def game_loop(self):
         while self.running:
-            # # TODO : update état du jeu
-            # state = {"type": "state", "players": [id(p) for p in self.players]}
-            # send_room(state)
-            # import time
-            # time.sleep(1)
-            pass
+            self.init_round()
+            sleep(5)
+            self.end_round()
 
-    def send_room(self, state):
+    def send_room(self, message: str | bytes):
         for p in self.players:
-            p.send(state)
+            p.send(message)
 
     def set_difficulty(self, client_caller, difficulty_code):
-        if self.admin == client_caller:
+        if not self.running and self.admin == client_caller:
             self.difficulty = difficulty_code
 
     def set_level(self, client_caller, level_code):
-        if self.admin == client_caller:
+        if not self.running and self.admin == client_caller:
             self.level = level_code
+
+    def init_round(self):
+        self.current_note = random.choice(notes)
+        self.score_round = {p: 0 for p in self.players}
+        self.round_audios = {p: None for p in self.players}
+        self.send_room(f"RS {self.current_note}")
+
+    def end_round(self):
+        for key, val in self.score_round.items():
+            self.score_game[key] += val
+
+        final_audio = mix_audios(self.round_audios.values())
+        self.send_room(f"AR {final_audio}")
+
+    def handle_client_audio(self, client_caller, audio_bytes: bytes):
+        if self.running:
+            self.round_audios[client_caller] = audio_bytes
+            score = compare_notes(audio_bytes, self.current_note)
+            self.score_round[client_caller] = score
